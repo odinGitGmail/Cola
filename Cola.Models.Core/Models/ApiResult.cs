644707f7bf491206e31d1d89736fe7c0170d14d9
odinsam.cn@gmail.com <odinsam.cn@gmail.com>
2024-09-11 15:08:17 +0800

    no message

--- Cola.Models.Core/Models/ApiResult.cs
+++ Cola.Models.Core/Models/ApiResult.cs
@@ -3,7 +3,6 @@ using System.Text;
 using Cola.Models.Core.Models.ColaAuthen;
 using Cola.Utils;
 using Cola.Utils.Enums;
-using Microsoft.AspNetCore.Mvc;
 using NetTaste;
 
 namespace Cola.Models.Core.Models;
